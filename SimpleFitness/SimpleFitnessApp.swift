acedfd3e2fc8d81003ca258c69f1a080f91dc160
flenhu <57570282+flenhu@users.noreply.github.com>
2024-05-23 16:49:40 -0700

    Initial Commit

---
+++ SimpleFitness/SimpleFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFitnessApp.swift
+//  SimpleFitness
+//
+//  Created by Frank Le-Nhu on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
